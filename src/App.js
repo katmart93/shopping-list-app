c5b5fb96d0a5ae2b991192bcd0e578d348c513d9
katmart93 <katwlodarczyk93@gmail.com>
2022-08-19 20:59:42 +0200

    list items mapped through in shoppinglists component

--- src/App.js
+++ src/App.js
@@ -9,8 +9,18 @@ import "./App.css";
 
 function App() {
   const [lists, setLists] = useState([
-    { title: "Home", date: "05.06.2022", id: uuidv4() },
-    { title: "Grill", date: "2.02.2022", id: uuidv4() },
+    {
+      title: "Home",
+      date: "05.06.2022",
+      id: uuidv4(),
+      listItems: ["sushi", "carrots", "apples"],
+    },
+    {
+      title: "Grill",
+      date: "2.02.2022",
+      id: uuidv4(),
+      listItems: ["bananas", "kiwis", "bread", "chips"],
+    },
   ]);
 
   return (
--- src/components/ShoppingLists.jsx
+++ src/components/ShoppingLists.jsx
@@ -2,9 +2,14 @@ export default function ShoppingLists({ lists }) {
   return (
     <div>
       {lists.map((list) => (
-        <div>
+        <div key={list.id}>
           <h2>{list.title}</h2>
           <p>{list.date}</p>
+          <ul>
+            {list.listItems.map((listItem) => (
+              <li key={listItem}>{listItem}</li>
+            ))}
+          </ul>
         </div>
       ))}
     </div>
