e830de28c32fdafddf708a7444a5c5acb12363e0
katmart93 <katwlodarczyk93@gmail.com>
2022-08-19 20:25:31 +0200

    shopping lists state created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 //components
 import Title from "./components/Title";
 // styles
 import "./App.css";
 
 function App() {
+  const [shoppingLists, setShoppingLists] = useState([
+    { title: "Home", date: "05.06.2022", id: uuidv4() },
+    { title: "Grill", date: "2.02.2022", id: uuidv4() },
+  ]);
+
   return (
     <div className="App">
       <Title />
