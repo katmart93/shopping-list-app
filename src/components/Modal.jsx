a62720b160e97db4285fe7839e8e0dcb28be4486
katmart93 <katwlodarczyk93@gmail.com>
2022-08-19 22:05:11 +0200

    form created in newlistform

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,14 @@
+// components
+import NewListForm from "./NewListForm";
 // styles
 import "./Modal.css";
 
 export default function Modal() {
-  return <div className="modal-backdrop"></div>;
+  return (
+    <div className="modal-backdrop">
+      <div className="modal">
+        <NewListForm />
+      </div>
+    </div>
+  );
 }
--- src/components/NewListForm.jsx
+++ src/components/NewListForm.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function NewListForm() {
-  return <div>NewListForm</div>;
+  return (
+    <form className="new-list-form">
+      <label>
+        <span>List title:</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Shopping date:</span>
+        <input type="date" />
+      </label>
+      <label>
+        <span>Items:</span>
+        <input type="text" />
+        <button>add</button>
+      </label>
+    </form>
+  );
 }
