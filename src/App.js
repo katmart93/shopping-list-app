25211abb2df2c87a519f142827e949795c8001f8
katmart93 <katwlodarczyk93@gmail.com>
2022-08-20 14:51:27 +0200

    routes created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //components
 import Home from "./pages/Home";
+import SingleList from "./pages/SingleList";
 // styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shopping-list" element={<SingleList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
