a8f79403283bfc047ee4c0ed9ba71f97f2deaa75
katmart93 <katwlodarczyk93@gmail.com>
2022-08-20 13:24:21 +0200

    items added to an array

--- src/components/NewListForm.jsx
+++ src/components/NewListForm.jsx
@@ -4,7 +4,9 @@ import { v4 as uuidv4 } from "uuid";
 export default function NewListForm({ addList }) {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
-  console.log(title, date);
+  const [item, setItem] = useState("");
+  const [currItems, setCurrItems] = useState([]);
+  console.log(title, date, item, currItems);
 
   const resetForm = () => {
     setTitle("");
@@ -17,10 +19,18 @@ export default function NewListForm({ addList }) {
       title: title,
       date: date,
       id: uuidv4(),
+      listItems: currItems,
     };
 
     addList(list);
     resetForm();
+  };
+
+  const addItem = (e) => {
+    e.preventDefault();
+
+    setCurrItems((prevItems) => [...prevItems, item]);
+    setItem("");
   };
 
   return (
@@ -43,8 +53,12 @@ export default function NewListForm({ addList }) {
       </label>
       <label>
         <span>Items:</span>
-        <input type="text" />
-        <button>add</button>
+        <input
+          type="text"
+          onChange={(e) => setItem(e.target.value)}
+          value={item}
+        />
+        <button onClick={addItem}>add</button>
       </label>
       <button>submit</button>
     </form>
