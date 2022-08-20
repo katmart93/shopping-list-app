33ded34afdbc4ef57e76c4a1ce63681e0dc611f9
katmart93 <katwlodarczyk93@gmail.com>
2022-08-20 17:56:22 +0200

    home component updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 // components
 import ShoppingLists from "../components/ShoppingLists";
 import AddListButton from "../components/AddListButton";
