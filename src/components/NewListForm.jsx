a4161e8b6f651e378ae50ab8adbe6bc0581bf31d
katmart93 <katwlodarczyk93@gmail.com>
2022-08-20 12:40:25 +0200

    title and date set to target value

--- src/components/NewListForm.jsx
+++ src/components/NewListForm.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import { useState } from "react";
 
 export default function NewListForm() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  console.log(title, date);
+
   return (
     <form className="new-list-form">
       <label>
         <span>List title:</span>
-        <input type="text" />
+        <input type="text" onChange={(e) => setTitle(e.target.value)} />
       </label>
       <label>
         <span>Shopping date:</span>
-        <input type="date" />
+        <input type="date" onChange={(e) => setDate(e.target.value)} />
       </label>
       <label>
         <span>Items:</span>
