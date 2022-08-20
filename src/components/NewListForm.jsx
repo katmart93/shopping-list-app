b867b5893d98a600e3a14a41729d05eaff0c9ab3
katmart93 <katwlodarczyk93@gmail.com>
2022-08-20 12:47:50 +0200

    handling submit of form

--- src/components/NewListForm.jsx
+++ src/components/NewListForm.jsx
@@ -5,15 +5,32 @@ export default function NewListForm() {
   const [date, setDate] = useState("");
   console.log(title, date);
 
+  const resetForm = () => {
+    setTitle("");
+    setDate("");
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    resetForm();
+  };
+
   return (
-    <form className="new-list-form">
+    <form className="new-list-form" onSubmit={handleSubmit}>
       <label>
         <span>List title:</span>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} />
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
       </label>
       <label>
         <span>Shopping date:</span>
-        <input type="date" onChange={(e) => setDate(e.target.value)} />
+        <input
+          type="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        />
       </label>
       <label>
         <span>Items:</span>
