923a7d5d2bab43c365aff474d2b0dca916113c91
katmart93 <katwlodarczyk93@gmail.com>
2022-08-21 18:50:24 +0200

    modal styles updated

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,5 +2,9 @@
 import "./Modal.css";
 
 export default function Modal({ children }) {
-  return <div className="modal-backdrop">{children}</div>;
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-wrapper">{children}</div>
+    </div>
+  );
 }
