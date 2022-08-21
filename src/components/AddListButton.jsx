be592298d80cd755ec825daa707a8c0aeb212ac2
katmart93 <katwlodarczyk93@gmail.com>
2022-08-21 18:09:30 +0200

    add list button and shopping list styles updated

--- src/components/AddListButton.jsx
+++ src/components/AddListButton.jsx
@@ -1,7 +1,17 @@
+// styles
+import "./AddListButton.css";
+// icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+
 export default function AddListButton({ setShowFormModal }) {
   return (
-    <div>
-      <button onClick={() => setShowFormModal(true)}>Add Shopping List</button>
+    <div className="plus-icon-wrapper">
+      <FontAwesomeIcon
+        icon={faCirclePlus}
+        onClick={() => setShowFormModal(true)}
+        className="plus-icon"
+      />
     </div>
   );
 }
